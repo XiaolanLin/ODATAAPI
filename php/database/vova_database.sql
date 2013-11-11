dc2ed21e1550f617aa85ca3a500108d8b010f86e
Man Yang <yang.ma@husky.neu.edu>
2013-11-11 16:02:11 -0500

    upload database

---
+++ php/database/vova_database.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 11, 2013 at 03:58 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `vova_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) NOT NULL,
+  `password` text NOT NULL,
+  `create_data` datetime NOT NULL,
+  `last_login_time` datetime NOT NULL,
+  `Last_name` varchar(255) DEFAULT NULL,
+  `First_name` varchar(255) DEFAULT NULL,
+  `Email_add` text,
+  `Gender` enum('F','M','N') DEFAULT NULL,
+  `Location` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `user_name`, `password`, `create_data`, `last_login_time`, `Last_name`, `First_name`, `Email_add`, `Gender`, `Location`) VALUES
+(1, 'admin', 'admin', '2013-11-11 15:56:22', '2013-11-11 00:00:00', NULL, 'vova', NULL, 'F', 'snell library');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
