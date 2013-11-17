c619d0296dd9504e4ee45b18a54e65adf947b5a1
dingt <ding.t@husky.neu.edu>
2013-11-17 17:54:29 -0500

    chage host user database

--- phplogin code/login_3.php
+++ phplogin code/login_3.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "vova_database";
+$mysql_user = "voya";
+$mysql_password = "VOYAvoya2013@";
+$mysql_database = "voya";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) 
 or die("Opps some thing went wrong");
 
