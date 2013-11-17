083ce696a8fe44f0fd32232d802df8ee458e4a2b
dingt <ding.t@husky.neu.edu>
2013-11-17 15:02:23 -0500

    updated

--- phplogin code/configuration_3.php
+++ phplogin code/configuration_3.php
@@ -1,6 +1,6 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "username";
+$mysql_user = “root”;
 $mysql_password = "";
 $mysql_database = "vova_database";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) 
