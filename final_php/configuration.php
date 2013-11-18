4c0e762f467a58c94f78d0dab73103d8524b9ee2
Xiaolan Lin <lin.xiaol@husky.neu.edu>
2013-11-18 18:44:16 -0500

    update

--- final_php/configuration.php
+++ final_php/configuration.php
@@ -1,4 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf370
+{\rtf1\ansi\ansicpg1252\cocoartf1265
 {\fonttbl\f0\fmodern\fcharset0 Courier;}
 {\colortbl;\red255\green255\blue255;}
 \margl1440\margr1440\vieww10800\viewh8400\viewkind0
@@ -6,9 +6,9 @@
 \pard\pardeftab720
 
 \f0\fs24 \cf0 <?php\
-$mysql_hostname = "localhost";\
-$mysql_user = \'93root\'94;\
-$mysql_password = "";\
+$mysql_hostname = "173.201.214.70";\
+$mysql_user = \'93voya\'94;\
+$mysql_password = \'93VOYAvoya2013@\'94;\
 $mysql_database = "voya";\
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) \
 or die("Opps some thing went wrong");\
